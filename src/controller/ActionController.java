3c81dd403c8e59fb1d64de74661c9ba4caf1a44a
Ahmad Abboush <ahmad.abboush@tu-dortmund.de>
2020-11-17 12:25:56 +0100

    some Changes in ActionController

--- src/controller/ActionController.java
+++ src/controller/ActionController.java
@@ -7,6 +7,7 @@ import java.util.Comparator;
 import model.Dice;
 import model.Player;
 
+
 public class ActionController {
 
 	private FarkleController farkleController;
@@ -16,48 +17,86 @@ public class ActionController {
 	}
 
 	public void choose(ArrayList<Dice> dices) {
-		farkleController.getCalculationController().calculate(dices) ;
+		farkleController.getCalculationController().calculate(dices);
 	}
 
 	public void chooseAll(ArrayList<Dice> dices) {
-		farkleController.getCalculationController().calculate(dices) ;
-	}
-	// TODO:1.sort,,alle einsen ist ok bis andere zahl gibt uzw solange alle richtig ausgewählte zeichnen kann man weiter
-	// TODO ueberpruefen wenn was nicht stimmt dann stop und arraylist leeren .
-	public void confirm(ArrayList<Dice> dices) {
-		sort(dices);
-		if(confirm1(dices)){
-			int sum = farkleController.getCalculationController().calculate(dices);
-			if(farkleController.getRoundController().isEndOfGame(farkleController.getFarkle().getCurrentGame().getCurrentRound()))
-				//TODO : was passiert im Fall end of game ..
-			if(farkleController.getRoundController().isEndOfRound(farkleController.getFarkle().getCurrentGame().getCurrentRound()))
-				farkleController.getRoundController().setNextRound();
-			if(farkleController.getRoundController().isEndOfTurn(farkleController.getFarkle().getCurrentGame().getCurrentPlayer()))
-				farkleController.getRoundController().setNextPlayer();
-			else {
-				farkleController.getFarkle().getCurrentGame().getCurrentPlayer().setRoundScore(sum + farkleController
-						.getFarkle().getCurrentGame().getCurrentPlayer().getRoundScore());
-				dices.clear();
-			}
-		}
-		else{
-			dices.clear();
-		}
-	}
-	private boolean confirm1 (ArrayList<Dice> dices){
-		if (dices.size() == 0 ) return true ;
-
-		else return false ;
-
-	}
-
-	private void sort(ArrayList<Dice> dices){
-		Collections.sort(dices, Comparator.comparingInt(Dice::getValue));
-
+		farkleController.getCalculationController().calculate(dices);
 	}
 
 	public void bank(Player player) {
+		if (player.getRoundScore() >= 250) {
+			farkleController.getFarkle().getCurrentGame().setCurrentPlayer(player);
+			farkleController.getRoundController().setNextPlayer();
+		}
+	}
+
+	public void confirm(ArrayList<Dice> dices) {
+		int sum = farkleController.getCalculationController().calculate(dices);
+		if (sum == 0) {
+			dices.clear();
+		} else {
+			Player player = farkleController.getFarkle().getCurrentGame().getCurrentPlayer();
+			if (farkleController.getRoundController().isEndOfGame(farkleController.getFarkle().getCurrentGame().getCurrentRound())) {
+				if(farkleController.getRoundController().isDraw(farkleController.getFarkle().getCurrentGame().getCurrentRound())){
+					farkleController.getRoundController().addExtraRound();
+				}
+				show();
+			}
+
+			if (farkleController.getRoundController().isEndOfRound(farkleController.getFarkle().getCurrentGame().getCurrentRound())) {
+				farkleController.getRoundController().setNextRound();
+			}
+
+			if (farkleController.getRoundController().isEndOfTurn(farkleController.getFarkle().getCurrentGame().getCurrentPlayer())) {
+				player.setRoundScore(sum);
+				farkleController.getRoundController().setNextPlayer();
+
+			}
+
+			else {
+				player.setRoundScore(sum);
+				if(player.getTakenDices() !=6) {
+					player.setDice(reduce(dices, player.getDice()));
+					player.setTakenDices(player.getTakenDices() + dices.size());
+				}
+				else {
+					player.setScore(sum + player.getScore());
+					resetPlayer(player);
+				}
+			}
+		}
+
+	}
+
+	private ArrayList<Dice> reduce(ArrayList<Dice> takenDice, ArrayList<Dice> reducedArray) {
+		if (reducedArray.size() >= takenDice.size()) {
+			int i = takenDice.size();
+			while (i >0){
+				reducedArray.remove(i);
+				i-- ;
+			}
+		}
+		return reducedArray;
+	}
+
+	private void resetPlayer (Player player){
+		player.setPlayed(false);
+		player.setRoundScore(0);
+		player.setTakenDices(0);
+		ArrayList<Dice> dices = player.getDice();
+		for(Dice dice : dices)
+			dice.setUsed(false);
+	}
+
+	private void show (){
+		ArrayList<Player> players = farkleController.getFarkle().getPlayer() ;
+		players.sort(Comparator.comparing(Player::getScore));
+		Collections.reverse(players);
+		players.stream().map(player -> player.getUserName() + ": " + player.getScore()).forEach(System.out::println);
 
 	}
 
 }
+
+
