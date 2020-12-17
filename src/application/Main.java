311840458981fed8de5f900346bb43e1aa7b4c35
KhaledYounes <70337512+KhaledYounes@users.noreply.github.com>
2020-12-17 05:00:47 -1200

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,43 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import view.SampleViewController;
+import view.MainViewController;
 
+import java.net.URL;
 
 public class Main extends Application {
+
 	@Override
 	public void start(Stage primaryStage) {
+
 		try {
-			SampleViewController sampleViewController = new SampleViewController();
-			Scene scene = new Scene(sampleViewController,400,200);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+			FXMLLoader loader = new FXMLLoader();
+			URL xmlUrl = getClass().getResource("../view/MainWindowView.fxml");
+			loader.setLocation(xmlUrl);
+			Parent root = loader.load();
+
+			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
+
+			primaryStage.setTitle("Farkle");
+			primaryStage.getIcons().add(new Image("application/icon.jpg"));
+
 			primaryStage.show();
+
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+
 	}
 	
 	public static void main(String[] args) {
 		launch(args);
 	}
+
 }
